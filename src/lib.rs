bca2ddb7b37693d6a22f84fe9b81e9b41c5ef38f
Lachezar Lechev <elpiel93@gmail.com>
2019-01-10 18:29:04 +0100

    is_valid_transition - implement and proper tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,22 @@ use num_bigint::BigUint;
 
 type BalanceHash = HashMap<String, BigUint>;
 
+pub fn is_valid_transition(channel_deposit: &BigUint, prev: &BalanceHash, next: &BalanceHash) -> bool {
+    let next_sum: BigUint = next.values().sum();
+
+    if &next_sum >= channel_deposit {
+        return false;
+    }
+
+    let prev_sum: BigUint = prev.values().sum();
+
+    if prev_sum > next_sum {
+        return false;
+    }
+
+    prev.iter().all(|(id, val)| next.contains_key(id) && next.get(id).unwrap() >= val)
+}
+
 pub fn get_health(our_balances: BalanceHash, new_balances: BalanceHash) -> bool {
     let threshold: BigUint = BigUint::from(950u32);
     let sum_our_balance: BigUint = our_balances.values().sum();
@@ -29,6 +45,57 @@ pub fn get_health(our_balances: BalanceHash, new_balances: BalanceHash) -> bool 
 
 #[cfg(test)]
 mod tests {
+    mod is_valid_transition {
+        use num_bigint::BigUint;
+
+        use crate::is_valid_transition;
+        use crate::BalanceHash;
+
+        #[test]
+        fn sum_of_next_values_should_not_exceed_the_channel_deposit() {
+            let channel_deposit = BigUint::from(20u32);
+            let mut next = BalanceHash::new();
+
+            next.insert("a".to_owned(), BigUint::from(10u64));
+            let prev = BalanceHash::new();
+
+            assert_eq!(true, is_valid_transition(&channel_deposit, &prev, &next));
+
+            // make the sum of next 30 which is > 20, it should fail the validation
+            next.insert("b".to_owned(), BigUint::from(20u64));
+            assert_eq!(false, is_valid_transition(&channel_deposit, &prev, &next));
+        }
+
+        #[test]
+        fn sum_of_next_values_should_be_ge_than_sum_of_prev_values() {
+            let channel_deposit = BigUint::from(100u32);
+
+            let mut next = BalanceHash::new();
+            next.insert("a".to_owned(), BigUint::from(30u64));
+            next.insert("b".to_owned(), BigUint::from(10u64));
+
+            let mut prev = BalanceHash::new();
+            prev.insert("a".to_owned(), BigUint::from(30u64));
+            prev.insert("b".to_owned(), BigUint::from(10u64));
+
+            assert_eq!(true, is_valid_transition(&channel_deposit, &prev, &next));
+
+            prev.insert("e".to_owned(), BigUint::from(20u64));
+            // no entry for "e", so it should fail t he validation
+            assert_eq!(false, is_valid_transition(&channel_deposit, &prev, &next));
+
+            // make the sum of prev 50 which is > 30, it should fail the validation
+            next.insert("e".to_owned(), BigUint::from(20u64));
+            assert_eq!(true, is_valid_transition(&channel_deposit, &prev, &next));
+
+            // make next "e" key smaller than the prev "e" key, it should fail the validation
+            *next.get_mut(&"e".to_owned()).unwrap() = BigUint::from(10u64);
+            // but also add another key to make the sum of next > prev!
+            next.insert("f".to_owned(), BigUint::from(30u64));
+            assert_eq!(false, is_valid_transition(&channel_deposit, &prev, &next));
+        }
+    }
+
     mod get_heath {
         use num_bigint::BigUint;
 
